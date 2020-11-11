cbfba9cac291a792f43bd282c2be3936b1d15a81
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2020-11-11 14:46:27 +0000

    Adding loading wait...

--- src/pages/SwapiFilmListPage.js
+++ src/pages/SwapiFilmListPage.js
@@ -3,32 +3,40 @@ import {SwapiFilmPage} from './SwapiFilmPage'
 
 export const SwapiFilmListPage = () => {
     const [films, setFilm] = useState({ film: {} });
-
-    useEffect(() => {
-        const fetchFilm = async () =>{
-            const response = await fetch('https://swapi.dev/api/films/');
-            const data = await response.json();
-            setFilm(data.results);
-            console.log(data);
-        }
-        fetchFilm();
-    }, [])
+    const [isLoaded, setIsLoaded]  = useState(false);
+    const [error, setError] = useState(null)
 
     // useEffect(() => {
-    //     fetch('https://swapi.dev/api/films/')
-    //     .then(res => res.json())
-    //     .then(
-    //         (result) => {
-    //             setFilm(result)
-    //             console.log(result)
-    //         }
-    //     )
-    // },[])
+    //     const fetchFilm = async () =>{
+    //         const response = await fetch('https://swapi.dev/api/films/');
+    //         const data = await response.json();
+    //         setFilm(data.results);
+    //         console.log(data);
+    //     }
+    //     fetchFilm();
+    // }, [])
+
+    useEffect(() => {
+        fetch('https://swapi.dev/api/films/')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true)
+                setFilm(result)
+                console.log(result)
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    },[])
 
     return(
         <>
             <h1>List of Films</h1>
-            <SwapiFilmPage films={films} />
+            <SwapiFilmPage films={films} error={error} isLoaded={isLoaded}/>
+            {/* <SwapiFilmPage films={films} /> */}
         </>
     )
 
--- src/pages/SwapiFilmPage.js
+++ src/pages/SwapiFilmPage.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-export const SwapiFilmPage = ({films}) => (
-    films.map(film => 
-        <div>
-            <h3> Title: {film.title} </h3>
-            <p> Director: {film.director} </p>
-            <p> Release date: {film.release_date}</p>
-        </div>
-    )
-)
+export const SwapiFilmPage = ({films, error, isLoaded}) => {
+    if(error) {
+        return <div>Error : {error.message}</div>
+    } else if (!isLoaded) {
+        return <div>Loading...</div>
+    } else {
+        return (
+            films.map(film => 
+                <div>
+                    <h3> Title: {film.title} </h3>
+                    <p> Director: {film.director} </p>
+                    <p> Release date: {film.release_date}</p>
+                </div>
+            )
+        )
+    }
+}
+
+// export const SwapiFilmPage = ({films}) => (
+//     films.map(film => 
+//         <div>
+//             <h3> Title: {film.title} </h3>
+//             <p> Director: {film.director} </p>
+//             <p> Release date: {film.release_date}</p>
+//         </div>
+//     )
+// )
     
